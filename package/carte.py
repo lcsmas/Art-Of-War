f8e6b213004f423912f1444fefc33aa2fc05d4d4
Lucas MAS <lucas.mas@igtp3-mac013.isim.intra>
2017-12-01 11:26:23 +0100

    01/12/2017 écriture des fonctions de carte.py

--- package/carte.py
+++ package/carte.py
@@ -1,70 +1,90 @@
 #coding:utf-8
 # === === Carte === === #
-    
+
+from mainJoueur import *
+
 def creerCarte(role):
     #Pre-condition : La carte renvoie un élément de type carte ayant comme role le role donné en parametre (role est un String)
     #Pre-condition : role est "Archer" ou "Garde" ou "Soldat" ou "Roi1" ou "Roi2" 
     #Post-condition : 
     #Resultat : 
-    
-    pass 
+    return { "role" : role, "estVerticale" : True, "positionCarte" : "Vide", "pointDegatSubit" : 0 } 
 
 def estVerticale(carte) :
     # Pre-condition : La carte est de type Carte.
     # Post-condition : Aucune
     # Resultat : Retourne True si la carte est verticale, retourne False sinon
-    
-    pass
+    return carte["estVerticale"]
 
 def changementMode(carte) : 
     # Pre-condition : la carte est de type Carte 
     # Post-condition : Aucune
     # Resultat : Le mode de la carte a été modifié :  Vertical si Horizontal et inversement. La carte est modifiée et renvoyée 
-    
-    pass
+    if carte["estVerticale"] == True :
+        carte["estVerticale"] = False
+    else :
+        carte["estVerticale"] = True
+    return carte
     
 
 def positionCarte(carte) :
     # Pre-condition : La carte est type Carte 
     # Post-condition : Aucune
     # Resultat : Retourne la chaine de caractère représentant la position de la carte sur le plateau ("F1","F2",...,"A3")  
-    
-    pass
+    return carte["positionCarte"]
     
 def setPositionCarte(carte,pos) : 
     # Pre-condition : La carte est type Carte, la pos est une string 
     # Post-condition : Aucune
     # Resultat : Modifie la position de la carte. Cette fonction renvoie aussi la carte modifiée
-    
-    pass
-    
-def pointAttaque(carte) : 
+    carte["positionCarte"] = pos
+    return carte    
+
+def pointAttaque(carte, main) : 
     # Pre-condition : la carte est de type Carte
     # Post-condition : Aucune
     # Resultat : retourne le nombre de point d'attaque de la carte
-    
-    pass
+    if roleCarte(carte) == "Soldat" :
+        return nbCarteMain(main)
+    return 1 #Toutes les cartes ont 1 d'attaque sauf le soldat
     
 def pointDefense(carte) : 
     # Pre-condition : la carte est de type Carte
     # Post-condition : Aucune
     # Resultat : retourne le nombre de point de defense de la carte EN FONCTION DE SON MODE 
-        
-    pass
+    role = roleCarte(carte)
+
+    if role in ["Archer", "Soldat"] : #Archer et Soldat ont le même nombre de points de défense
+        if estVerticale(carte) :
+            return 2
+        return 1
+
+    if role == "Garde":
+        if estVerticale(carte) :
+            return 3
+        return 2
+
+    if role == "Roi1" : #Le nombre de points de défenses du Roi1 ne dépend pas de sa posture 
+        return 4
+
+    if role == "Roi2" :
+        if estVerticale(carte) :
+            return 5
+        return 4
+    
     
 def pointDegat(carte) : 
     # Pre-condition : la carte est de type Carte
     # Post-condition : Aucune
     # Resultat : retourne le nombre de point de dégat subits de la carte
-    
-    pass
+    return carte["pointDegatSubit"]
     
 def capture(carte,joueur) : 
     # Pre-condition : la carte est de type Carte. le joueur est de type Joueur
     # Post-condition : Aucune
     # Resultat : la carte devient capturée. La carte est placée dans le Royaume du joueur entrée en parametre
-    
-    pass
+    roy = royaume(joueur)
+    entrerRoyaume(roy, carte)
     
 def setPointDegat(carte,nouvDegat) :
     # Pre-condition : La carte est de type Carte, nouvDegat est de type Int. 
